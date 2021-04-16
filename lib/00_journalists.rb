d3741c824d5181227d0554ff96b59421d7b88b28
maryboulanger <ma.boulanger@skema.edu>
2021-04-16 14:42:20 +0200

    suite correction

---
+++ lib/00_journalists.rb
@@ -0,0 +1,95 @@
+#À partir de cette array, code un programme ruby qui répondra aux questions suivantes :
+
+#    Combien y a-t-il de handle dans cette array ?
+
+def journalists_number(journalists)
+  puts "1. Il y a #{journalists.length} journalistes dans cette liste."
+end
+
+#    Quel est le handle le plus court de cette liste ?
+def shorter_pseudo(journalists)
+  puts "2.Le plus court pseudo de cette liste est :"
+  print "> "
+  puts journalists.min_by { |v| v.length}
+end
+
+#    Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+
+def pseudo_withfiveletters(journalists)
+  list_with5 = journalists.select{ |v| v.size == 6 }
+  num_with5 = list_with5.count
+  puts "3. Les pseudos contenant 5 caractères (@ exclu) sont au nombre de:"
+  puts "> #{num_with5}"
+end
+
+#    Combien commencent par une majuscule (première lettre juste après le @) ?
+#fonctionne aussi >  puts journalists.select { |x| x =~ /^@[A-Z]/ }
+def pseudo_withmaj(journalists)
+  list_withmaj = journalists.select { |v| v.start_with? /^@[A-Z]/ }
+  num_withmaj = list_withmaj.count
+  puts "4. Les pseudos commençant par une majuscule sont au nombre de :"
+  puts "> #{num_withmaj}"
+end
+
+#    Trie la liste de handle par ordre alphabétique.
+def sorted_list_az(journalists)
+  ordre = journalists.sort_by {|journalists| journalists.upcase}
+  puts "5. Voici la liste dans l'ordre alphabétique :"
+  puts
+  puts ordre
+end
+
+#    Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+def sorted_list_size(journalists)
+  puts "6. Voici la liste du plus court au plus long pseudo :"
+  puts
+  puts journalists.sort_by {|v| v.size}
+end
+
+#    Quelle est la position dans l'array de la personne @epenser ?
+def find_index_epenser(journalists)
+  puts "7. Le pseudo est en position :"
+  print ">"
+  puts journalists.find_index("@epenser")
+end
+
+#    Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+def size_distribution(journalists)
+  #defini le cadre du plus petit au plus grand nb de caractère - ici de 1 à 33
+  shorter_size = 1
+  longer_size = (journalists.max_by { |v| v.length}).size
+
+  ##affiche les pseudos avec une taille donnée, faire.count pour avoir le nb de pseudo concerné
+  shorter_pseudo = journalists.select { |v| v.size == shorter_size }
+
+  puts "8.Voici la répartition des pseudos par taille :"
+  while shorter_size <= longer_size
+    puts "Il y a #{shorter_pseudo.count} pseudo de #{shorter_size} caractère."
+    shorter_size = shorter_size +1
+    shorter_pseudo = journalists.select { |v| v.size == shorter_size }
+  end
+end
+
+def perform
+
+journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+  puts "Les réponses sont :"
+  puts journalists_number(journalists)
+  puts
+  puts shorter_pseudo(journalists)
+  puts
+  puts pseudo_withfiveletters(journalists)
+  puts
+  puts pseudo_withmaj(journalists)
+  puts
+  puts sorted_list_az(journalists)
+  puts
+  puts sorted_list_size(journalists)
+  puts
+  puts find_index_epenser(journalists)
+  puts
+  print size_distribution(journalists)
+end
+
+perform
